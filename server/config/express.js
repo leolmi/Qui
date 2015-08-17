a807a998ca32bd40afd5c2ba0f3bfa0ff5c3e70b
leolmi <leo.olmi@gmail.com>
2015-08-17 10:53:48 +0200

    ottimizzazioni

--- server/config/express.js
+++ server/config/express.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var express = require('express');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
@@ -28,19 +28,32 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
-  if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
+
+  //if ('production' === env) {
+  //  app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+  //  app.use(express.static(path.join(config.root, 'public')));
+  //  app.set('appPath', config.root + '/public');
+  //  app.use(morgan('dev'));
+  //}
+  //
+  //if ('development' === env || 'test' === env) {
+  //  app.use(require('connect-livereload')());
+  //  app.use(express.static(path.join(config.root, '.tmp')));
+  //  app.use(express.static(path.join(config.root, 'client')));
+  //  app.set('appPath', 'client');
+  //  app.use(morgan('dev'));
+  //  app.use(errorHandler()); // Error handler - has to be last
+  //}
+
+  if (process.env.DEBUG=='debug') {
+    app.use(require('connect-livereload')());
   }
 
-  if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
-    app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
-    app.use(morgan('dev'));
+  app.use(express.static(path.join(config.root, 'client')));
+  app.set('appPath', 'client');
+  app.use(morgan('dev'));
+
+  if (process.env.DEBUG=='debug') {
     app.use(errorHandler()); // Error handler - has to be last
   }
 };
